a4c0883dd6157894c0006df2bd2597e4964b36ba
coder-showkat <showkat.coder@gmail.com>
2023-04-12 00:55:24 +0600

    initial commit

---
+++ app.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const bodyParser = require('body-parser');
+const app = express();
+const meals = require("./meals.json");
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.get("/meals", (req, res) => {
+    const page = req.query.page;
+    const limit = req.query.limit;
+    const search = req.query.search;
+
+    let displayMeals = [];
+
+    if (search) {
+        displayMeals = [...(meals.meals.filter(meal=>meal.strMeal.toLowerCase().includes(search.toLowerCase())))];
+    } else {
+        displayMeals = [...(meals.meals)];
+    }
+
+    if (page && limit) {
+    displayMeals = displayMeals.slice((page * limit) - limit, page * limit);  
+    } else if (page && limit === false) {
+        displayMeals = displayMeals.slice((page * 9 )- 9, page * 9);
+    } else if (page === false && limit) {
+        displayMeals = displayMeals.slice(0, limit);
+    } else if (!page && !limit) {
+        displayMeals = displayMeals.slice(0, 100);
+    }
+
+    res.send({meals: [...displayMeals]});
+})
+
+app.get("/meal/:id", (req, res)=>{
+    const id = req.params.id;
+    const meal = meals.meals.find(item=>item.idMeal === id);
+    
+    if(meal) res.send({meals: [{...meal}]})
+    else {
+        res.send({meals:null});
+    };
+})
+
+module.exports = app;
