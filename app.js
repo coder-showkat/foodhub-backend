a50226948b812b7e63a33d3a6f30ba96f4e221f1
coder-showkat <showkat.coder@gmail.com>
2023-04-12 02:04:04 +0600

    adding cors

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require("express");
+var cors = require('cors');
 const bodyParser = require('body-parser');
 const app = express();
 const meals = require("./meals.json");
+
+app.use(cors())
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
